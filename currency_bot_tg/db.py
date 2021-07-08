d52d3fa4b96bf0a44b215ab2f2185462ac0e3805
SergeiP-JS <forstudiesp@gmail.com>
2021-07-09 00:11:03 +0500

    new commit

---
+++ currency_bot_tg/db.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'SPridannikov'
+
+import datetime as DT
+import time
+
+from typing import Type
+
+# pip install peewee
+from peewee import (
+    DateField, DecimalField, BooleanField,
+    Model, TextField, ForeignKeyField, CharField, IntegerField, DateTimeField
+)
+from playhouse.sqliteq import SqliteQueueDatabase
+
+from config import DB_FILE_NAME
+from common import shorten
+
+# This working with multithreading
+# SOURCE: http://docs.peewee-orm.com/en/latest/peewee/playhouse.html#sqliteq
+db = SqliteQueueDatabase(
+    DB_FILE_NAME,
+    pragmas={
+        'foreign_keys': 1,
+        'journal_mode': 'wal',  # WAL-mode
+        'cache_size': -1024 * 64  # 64MB page-cache
+    },
+    use_gevent=False,  # Use the standard library "threading" module.
+    autostart=True,
+    queue_max_size=64,  # Max. # of pending writes that can accumulate.
+    results_timeout=5.0  # Max. time to wait for query to be executed.
+)
+
+
+class BaseModel(Model):
+    """
+    Базовая модель для классов-таблиц
+    """
+
+    class Meta:
+        database = db
+
+    def get_new(self) -> Type['BaseModel']:
+        return type(self).get(self._pk_expr())
+
+    def __str__(self):
+        fields = []
+        for k, field in self._meta.fields.items():
+            v = getattr(self, k)
+
+            if isinstance(field, (TextField, CharField)):
+                if v:
+                    v = repr(shorten(v))
+
+            elif isinstance(field, ForeignKeyField):
+                k = f'{k}_id'
+                if v:
+                    v = v.id
+
+            fields.append(f'{k}={v}')
+
+        return self.__class__.__name__ + '(' + ', '.join(fields) + ')'
+
+
+
+class ExchangeRate(BaseModel):
+    date = DateField()
+    value = DecimalField()
+
+
+class Subscription(BaseModel):
+    chat_id = IntegerField()
+    is_active = BooleanField()
+    was_sending = BooleanField()
+    creation_datetime = DateTimeField(default=DT.datetime.now)
+    modification_datetime = DateTimeField(default=DT.datetime.now)
+
+
+db.connect()
+db.create_tables([ExchangeRate, Subscription])
+
+# Задержка в 50мс, чтобы дать время на запуск SqliteQueueDatabase и создание таблиц
+# Т.к. в SqliteQueueDatabase запросы на чтение выполняются сразу, а на запись попадают в очередь
+time.sleep(0.050)
+
+if __name__ == '__main__':
+    pass
+    print(ExchangeRate.select().count())
+    print(ExchangeRate.get(ExchangeRate.id == 1).value)
+
+    # Subscription.truncate_table()
+    for s in Subscription.select():
+        print(s.id, s.chat_id, s.was_sending)
+
---
+++ currency_bot_tg/main.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'SPridannikov'
+import os
+import time
+import datetime as DT
+from threading import Thread
+import db
+from parser_ import parser_
+from run_check_subscriptions import check_
+
+
+# pip install python-telegram-bot
+from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, Bot, ParseMode
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, CallbackContext,CallbackQueryHandler
+from telegram.ext.dispatcher import run_async
+
+from config import TOKEN
+from common import get_logger, log_func, reply_error
+
+
+log = get_logger(__file__)
+
+COMMANDS = [['Подписаться'],['Последнее значение', 'За неделю', 'За месяц']]
+REPLY_KEYBOARD_MARKUP = ReplyKeyboardMarkup(COMMANDS, resize_keyboard=True)
+@run_async
+@log_func(log)
+def on_start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        f'Приветсвую {update.effective_user.first_name} &#128578;&#9995;'
+        'Данный бот способен отслежтвать USD валюту и отправлять вам уведомление при изменении $.'
+        ' С помощью меню вы можете подписаться/отписаться от рассылки, узнать актуальный курс за день, неделю или месяц.',
+        parse_mode=ParseMode.HTML,
+        reply_markup=REPLY_KEYBOARD_MARKUP
+    )
+@run_async
+@log_func(log)
+def on_reply_command(update: Update, context: CallbackContext):
+    message = update.message
+
+    message.reply_text(
+        'Reply command: ' + message.text,
+        reply_markup=REPLY_KEYBOARD_MARKUP
+    )
+
+
+@run_async
+@log_func(log)
+def on_command_up(update: Update, context: CallbackContext):
+    message = update.message
+    # print(message.text)
+    for s in db.Subscription.select():
+
+        if s.chat_id == update.effective_chat.id:
+            if s.is_active and message.text=="Подписаться":
+                message.text = "Подписка уже оформлена &#129300;"
+            else:
+                if s.is_active==False and message.text=="Отписаться":
+                    message.text = "Подписка не оформлена &#129300;"
+                else:
+                    if s.is_active and message.text == "Отписаться":
+                        s.is_active = 0
+                        s.save()
+                        s.was_sending = 0
+                        s.save()
+                        COMMANDS[0][0] = "Подписаться"
+                        message.text = "Вы успешно отписались &#128532;"
+                    else:
+                        if s.is_active == False and message.text == "Подписаться":
+                            s.is_active = 1
+                            s.save()
+                            s.modification_datetime = DT.datetime.now()
+                            s.save()
+                            COMMANDS[0][0] = "Отписаться"
+                            message.text = "Вы успешно подписались &#128521;"
+
+
+
+            break
+
+
+    if not db.Subscription.select().where(db.Subscription.chat_id == update.effective_chat.id):
+            db.Subscription.create(chat_id=update.effective_chat.id, is_active='true', was_sending='false')
+
+    message.reply_text(
+        message.text,
+        parse_mode=ParseMode.HTML,
+        reply_markup=ReplyKeyboardMarkup(COMMANDS, resize_keyboard=True)
+    )
+
+
+@run_async
+@log_func(log)
+def on_command_0(update: Update, context: CallbackContext):
+    if db.ExchangeRate.select().first():
+
+        message = update.message
+        message.reply_text(
+            f'Актуальный курс USD за <b><u>{db.ExchangeRate.get(db.ExchangeRate.id == db.ExchangeRate.select().count()).date}</u></b>: {db.ExchangeRate.get(db.ExchangeRate.id == db.ExchangeRate.select().count()).value}&#8381;',
+            parse_mode=ParseMode.HTML,
+            reply_markup=ReplyKeyboardMarkup(COMMANDS, resize_keyboard=True)
+        )
+    else:
+        message = update.message
+        message.reply_text(
+            'Бот ещё молодой и не имеет достаточно информации &#128532;',
+            parse_mode=ParseMode.HTML,
+            reply_markup=ReplyKeyboardMarkup(COMMANDS, resize_keyboard=True)
+        )
+
+@run_async
+@log_func(log)
+def on_command_1(update: Update, context: CallbackContext):
+        flag=0
+        arr=[]
+        i=0
+        for val in reversed(db.ExchangeRate.select()):
+            if i==7:
+                flag=1
+                break
+            arr.append(val.value)
+            i+=1
+        if flag:
+            message = update.message
+            message.reply_text(
+                f'Среднее USD за <b><u>неделю</u></b>: {float(sum(arr)) / max(len(arr), 1)}&#8381;',
+                parse_mode=ParseMode.HTML,
+                reply_markup=ReplyKeyboardMarkup(COMMANDS, resize_keyboard=True)
+            )
+        else:
+            message = update.message
+            message.reply_text(
+                'Бот ещё молодой и не имеет достаточно информации &#128532;',
+                parse_mode=ParseMode.HTML,
+                reply_markup=ReplyKeyboardMarkup(COMMANDS, resize_keyboard=True)
+            )
+
+@run_async
+@log_func(log)
+def on_command_2(update: Update, context: CallbackContext):
+        flag=0
+        arr = []
+        i = 0
+        for val in reversed(db.ExchangeRate.select()):
+            if i == 30:
+                flag = 1
+                break
+            arr.append(val.value)
+            i += 1
+        if flag:
+            message = update.message
+            message.reply_text(
+                f'Среднее USD за <b><u>месяц</u></b>: {float(sum(arr)) / max(len(arr), 1)}&#8381;',
+                parse_mode=ParseMode.HTML,
+                reply_markup=ReplyKeyboardMarkup(COMMANDS, resize_keyboard=True)
+            )
+        else:
+            message = update.message
+            message.reply_text(
+                'Бот ещё молодой и не имеет достаточно информации &#128532;',
+                parse_mode=ParseMode.HTML,
+                reply_markup=ReplyKeyboardMarkup(COMMANDS, resize_keyboard=True)
+            )
+
+
+
+@run_async
+@log_func(log)
+def on_request(update: Update, context: CallbackContext):
+    message = update.effective_message
+
+    text = message.text
+
+    message.reply_text(text)
+
+
+def on_error(update: Update, context: CallbackContext):
+    reply_error(log, update, context)
+
+
+def main():
+
+            cpu_count = os.cpu_count()
+            workers = cpu_count
+            log.debug('System: CPU_COUNT=%s, WORKERS=%s', cpu_count, workers)
+
+            log.debug('Start')
+
+            updater = Updater(
+                TOKEN,
+                workers=workers,
+                use_context=True
+            )
+            Thread(target=check_,args=(updater,)).start()
+            dp = updater.dispatcher
+
+            # Кнопки
+            dp.add_handler(CommandHandler('start', on_start))
+            dp.add_handler(MessageHandler(Filters.text('Подписаться'), on_command_up))
+            dp.add_handler(MessageHandler(Filters.text('Отписаться'), on_command_up))
+            dp.add_handler(MessageHandler(Filters.text(COMMANDS[1][0]), on_command_0))
+            dp.add_handler(MessageHandler(Filters.text(COMMANDS[1][1]), on_command_1))
+            dp.add_handler(MessageHandler(Filters.text(COMMANDS[1][2]), on_command_2))
+            dp.add_handler(MessageHandler(Filters.text, on_request))
+
+            dp.add_error_handler(on_error)
+
+            updater.start_polling()
+            updater.idle()
+
+            log.debug('Finish')
+def pars():
+    while True:
+        parser_()
+        time.sleep(8*3600)
+
+
+# def check_1():
+#     updater = Updater(TOKEN)
+#     bot: Bot = updater.bot
+#
+#     while True:
+#         bot.send_message(973083137,"Привет")
+#         time.sleep(2)
+
+
+if __name__ == '__main__':
+     # asyncio.run(async_())
+     Thread(target=pars).start()
+     #
+     while True:
+         try:
+             main()
+         except:
+             log.exception('')
+
+             timeout = 15
+             log.info(f'Restarting the bot after {timeout} seconds')
+             time.sleep(timeout)
+
