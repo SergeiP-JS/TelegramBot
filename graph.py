7aed78fd00d30a65d859df9fdb6702b5dec223c5
SergeiP-JS <forstudiesp@gmail.com>
2021-07-26 12:46:14 +0500

    Доработка graph.py

--- graph.py
+++ graph.py
@@ -1,41 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__author__ = 'SPridannikov'
-
-
-import math
 import os
-
+from typing import List
 
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from matplotlib import ticker
 
-# data = {'series1':[1,3,4,3,5],
-#         'series2':[2,4,5,2,4],
-#         'series3':[3,2,3,1,3]}
+import db
+from config import PATH_GRAPH_WEEK,PATH_GRAPH_MONTH
 
 
-def create_graph(days,items):
-        # print(days,items)
-        data = {'USD':items}
-        df = pd.DataFrame(data)
-        x = np.arange(start=1, stop=days+1)
+def create_graph(items: List[db.ExchangeRate]):
+    days = len(items)
 
-        plt.axis([1,days+1,int(math.floor(min(items))),int(math.ceil(max(items)))])
-        plt.plot(x,df)
-        # plt.legend(data, loc=1)
-        # plt.show()
-        if not os.path.isdir('img'):
-                os.mkdir('img')
+    x = range(1, days + 1)
+    y = [x.value for x in items]
+    plt.plot(x, y)
 
-        if os.path.isfile(f'img/graph_{days}.png'):
-                os.remove(f'img/graph_{days}.png')
-                plt.savefig(f'img/graph_{days}.png',transparent=True,bbox_inches='tight')
-        else:
-                plt.savefig(f'img/graph_{days}.png', transparent=True, bbox_inches='tight')
+    path_graph=PATH_GRAPH_WEEK
+
+    if days==30:
+        path_graph = PATH_GRAPH_MONTH
+
+    os.makedirs('img', exist_ok=True)
+    plt.savefig(path_graph, transparent=True, bbox_inches='tight')
 
 
-# create_graph(3,[2,4,5])
+if __name__ == '__main__':
+    items = db.ExchangeRate.get_last_by(days=7)
+    create_graph(items)
